57fb89f1d825f6e47b20667cbfc74f173b7955d3
DevOp972 <100531674+devSmarak972@users.noreply.github.com>
2023-02-12 09:48:36 +0530

    changes

--- src/routes/competitions.js
+++ src/routes/competitions.js
@@ -7,21 +7,35 @@ var bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
 const fetchuser = require("../middlewares/fetchUser");
 require("dotenv").config();
+var ObjectId = require("mongoose").Types.ObjectId;
 
 // ROUTE 1:
 //Get all the notes using: GET "/api/comp/fetchallcomp". login required
-router.get("/fetchallcomp",  async (req, res) => {
+router.get("/fetchallcomp", async (req, res) => {
   try {
-    const comp = await Competition.find({}).populate('user');
+    const comp = await Competition.find({}).populate("user");
     res.json(comp);
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Internal Error occured");
   }
 });
-router.get("/fetchcomp/:id",  async (req, res) => {
+router.get("/fetchcomp/:id", async (req, res) => {
   try {
-    const comp = await Competition.find({id:req.params.id}).populate('user');
+    const comp = await Competition.find({ id: req.params.id }).populate("user");
+    res.json(comp);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Error occured");
+  }
+});
+router.get("/usercomp/:id", async (req, res) => {
+  console.log("here", req.params.id, typeof req.params.id);
+  try {
+    const comp = await Competition.find({
+      user: ObjectId(req.params.id),
+    }).populate("user");
+    console.log(comp);
     res.json(comp);
   } catch (error) {
     console.error(error.message);
@@ -57,26 +71,30 @@ router.post(
         desc,
         incentives,
         url,
-        uid,status,domain
-       
+        uid,
+        status,
+        domain,
       } = req.body;
       // console.log(Date.now(),new Date(Date.parse(regDeadline)));
       // req.body["status"]=Date.now()>(new Date(Date.parse(regDeadline))).now()?"Live":"Expired";
-      req.body["status"]="Live";
+      req.body["status"] = "Live";
       // is there are errors, return bad request and the errors
       const errors = validationResult(req);
-      console.log("here",req.body);
+      console.log("here", req.body);
       if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
       }
-      let comp = await Competition.findOne({ title: req.body.title });
+      let comp = await Competition.findOne({
+        title: req.body.title,
+        user: uid,
+      });
       if (comp) {
-        return res
-          .status(400)
-          .json({errors: "Sorry a competition already exist with this title " });
+        return res.status(400).json({
+          errors: "Sorry a competition already exist with this title ",
+        });
       }
       console.log(req.body.uid);
-       comp = new Competition({
+      comp = new Competition({
         img,
         title,
         orgName,
@@ -100,11 +118,13 @@ router.post(
 
       const savedComp = await comp.save();
 
-      postinguser=await User.findById(uid);
-      if(postinguser)
-      {
-      postinguser.posted.push({competition:savedComp._id, team:[{uid}]});
-      postinguser.save();
+      postinguser = await User.findById(uid);
+      if (postinguser) {
+        postinguser.posted.push({
+          competition: savedComp._id,
+          team: [uid],
+        });
+        postinguser.save();
       }
 
       console.log(postinguser);
@@ -117,167 +137,169 @@ router.post(
   }
 );
 
-
 //ROUTE 3
 //Update an existing competition using: POST "/api/comp/updatecomp/:id". login required
 
 router.put("/updatecomp/:id", fetchuser, async (req, res) => {
-    try {
-        const {
-            img,
-            title,
-            orgName,
-            modeOfEvent,
-            visibility,
-            eligibility,
-            priceMoney,
-            teamSize,
-            fee,
-            regDeadline,
-            startDate,
-            endDate,
-            rewards,
-            description,
-          } = req.body;
-  
-      // Create a newComp object
-      const newComp = {};
-  
-      if (title) {
-        newComp.title = title;
-      }
-      if (description) {
-        newComp.description = description;
-      }
-      if (img) {
-        newComp.img = img;
-      }
-      if (orgName) {
-        newComp.orgName = orgName;
-      }
-      if (modeOfEvent) {
-        newComp.modeOfEvent = modeOfEvent;
-      }
-      if (visibility) {
-        newComp.visibility = visibility;
-      }
-      if (eligibility) {
-        newComp.eligibility =eligibility;
-      }
-      if (priceMoney) {
-        newComp.priceMoney = priceMoney;
-      }
-      if (teamSize) {
-        newComp.teamSize = teamSize;
-      }
-      if (fee) {
-        newComp.fee = fee;
-      }
-      if (regDeadline) {
-        newComp.regDeadline = tag;
-      }
-      if (endDate) {
-        newComp.endDate = endDate;
-      }
-      if (rewards) {
-        newComp.rewards = rewards;
-      }
-  
-      // Find the competition to be updfated and update it
-      let comp = await Competition.findById(req.params.id);
-      if (!comp) {
-        return res.status(404).send("Not found");
-      }
-  
-      if (comp.user.toString() !== req.user.id) {
-        return res.status(401).send("Not Allowed");
-      }
-  
-      comp = await Competition.findByIdAndUpdate(
-        req.params.id,
-        { $set: newComp },
-        { new: true }
-      );
-      res.json({ comp });
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Internal Error occured");
+  try {
+    const {
+      img,
+      title,
+      orgName,
+      modeOfEvent,
+      visibility,
+      eligibility,
+      priceMoney,
+      teamSize,
+      fee,
+      regDeadline,
+      startDate,
+      endDate,
+      rewards,
+      description,
+    } = req.body;
+
+    // Create a newComp object
+    const newComp = {};
+
+    if (title) {
+      newComp.title = title;
     }
-  });
+    if (description) {
+      newComp.description = description;
+    }
+    if (img) {
+      newComp.img = img;
+    }
+    if (orgName) {
+      newComp.orgName = orgName;
+    }
+    if (modeOfEvent) {
+      newComp.modeOfEvent = modeOfEvent;
+    }
+    if (visibility) {
+      newComp.visibility = visibility;
+    }
+    if (eligibility) {
+      newComp.eligibility = eligibility;
+    }
+    if (priceMoney) {
+      newComp.priceMoney = priceMoney;
+    }
+    if (teamSize) {
+      newComp.teamSize = teamSize;
+    }
+    if (fee) {
+      newComp.fee = fee;
+    }
+    if (regDeadline) {
+      newComp.regDeadline = tag;
+    }
+    if (endDate) {
+      newComp.endDate = endDate;
+    }
+    if (rewards) {
+      newComp.rewards = rewards;
+    }
 
+    // Find the competition to be updfated and update it
+    let comp = await Competition.findById(req.params.id);
+    if (!comp) {
+      return res.status(404).send("Not found");
+    }
 
+    if (comp.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
 
-  //ROUTE 4
+    comp = await Competition.findByIdAndUpdate(
+      req.params.id,
+      { $set: newComp },
+      { new: true }
+    );
+    res.json({ comp });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Error occured");
+  }
+});
+
+//ROUTE 4
 //Delete an existing Competition using: POST "/api/comp/deletecomp/:id". login required
 
 router.delete("/deletecomp/:id", fetchuser, async (req, res) => {
-    try {
-  
-      // Find the competition to be deleted and delete it
-      let comp = await Competition.findById(req.params.id);
-      if (!comp) {
-        return res.status(404).send("Not found");
-      }
-  
-      // Allow deletion only if user owns this Note
-      if (comp.user.toString() !== req.user.id) {
-        return res.status(401).send("Not Allowed");
-      }
-  
-      comp = await Competition.findByIdAndDelete(req.params.id);
-  
-      res.json({ Success: "Note has been deleted", comp: comp });
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Internal Error occured");
+  try {
+    // Find the competition to be deleted and delete it
+    let comp = await Competition.findById(req.params.id);
+    if (!comp) {
+      return res.status(404).send("Not found");
     }
-  });
-  router.post("/apply",async (req, res) => {
-    try {
-  
-      // Find the competition to be deleted and delete it
-      console.log(req.body);
-      let comp = await Competition.findById(req.body.compid);
-      if (!comp) {
-        return res.status(404).send("Not found");
-      }
-      console.log(comp)
-     
-      applyinguser=await User.findById(req.body.uid);
-      applyinguser.applied.push({competition:comp._id,accepted:false,message:""});
+
+    // Allow deletion only if user owns this Note
+    if (comp.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+
+    comp = await Competition.findByIdAndDelete(req.params.id);
+
+    res.json({ Success: "Note has been deleted", comp: comp });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Error occured");
+  }
+});
+router.post("/apply", async (req, res) => {
+  try {
+    // Find the competition to be deleted and delete it
+    console.log(req.body);
+    let comp = await Competition.findById(req.body.compid);
+    if (!comp) {
+      return res.status(404).send("Not found");
+    }
+    console.log(comp);
+
+    applyinguser = await User.findById(req.body.uid);
+    applyinguser.applied.push({
+      competition: comp._id,
+      accepted: false,
+      message: "",
+    });
     applyinguser.save();
 
-      console.log(applyinguser)
+    console.log(applyinguser);
 
-      res.json({ comp });
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Internal Error occured");
-    }
-  });
+    res.json({ comp });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Error occured");
+  }
+});
 module.exports = router;
 
-  router.post("/apply",async (req, res) => {
-    try {
-  
-      // Find the competition to be deleted and delete it
-      console.log(req.body);
-      let comp = await Competition.findById(req.body.compid);
-      if (!comp) {
-        return res.status(404).send("Not found");
-      }
-      console.log(comp)
-     
-      applyinguser=await User.findById(req.body.uid);
-      applyinguser.applied.push({competition:comp._id,accepted:false,message:""});
+router.post("/apply", async (req, res) => {
+  try {
+    // Find the competition to be deleted and delete it
+    console.log(req.body);
+    let comp = await Competition.findById(req.body.compid);
+    if (!comp) {
+      return res.status(404).send("Not found");
+    }
+    console.log(comp);
+
+    applyinguser = await User.findById(req.body.uid);
+    applyinguser.applied.push({
+      competition: comp._id,
+      accepted: false,
+      message: "",
+    });
     applyinguser.save();
 
-      console.log(applyinguser)
+    console.log(applyinguser);
 
-      res.json({ comp });
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Internal Error occured");
-    }
-  });
+    res.json({ comp });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Error occured");
+  }
+});
 module.exports = router;
